9a5af4ff0badb97221400ca5a061ac5d0eb52a4d
isaolmez <isaolmez@gmail.com>
2017-04-12 14:09:40 +0300

    modified static resource example

--- spring-boot-web-static/src/main/java/com/isa/web/Application.java
+++ spring-boot-web-static/src/main/java/com/isa/web/Application.java
@@ -5,15 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-@SpringBootApplication(scanBasePackages = "com.isa")
-public class Application extends SpringBootServletInitializer {
+@SpringBootApplication
+public class Application {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Application.class);
-	}
-
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
 }
